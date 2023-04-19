db6f2c8fd47a1666025e3280c54b45612c1cb3ef
mrbenc88 <mrbenc88@students.cs.ubc.ca>
2023-04-19 03:05:24 -0700

    modify page titles

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Tech Career Change</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <body>
         <Main />
         <NextScript />
